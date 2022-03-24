aeeb6a30a0a874091ef55ffac905ddc07dd409dc
Thales Felipe Torquato Suehara <suhthales@gmail.com>
2022-03-24 18:46:12 -0300

    fazendo uma superclasse abstract da calculadora

---
+++ calculadora 1/src/br/com/treinaweb/calculadora/classes/CalculoMatematico.java
@@ -0,0 +1,19 @@
+package br.com.treinaweb.calculadora.classes;
+
+import br.com.treinaweb.calculadora.interfaces.calculadorMatematico;
+
+public class CalculoMatematico implements calculadorMatematico {
+
+	@Override
+	public boolean validar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int calcular() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
